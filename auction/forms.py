fa84be7ffea5a72a45d725d19f18f9b0227ce5db
alex-hofsteede <alex@hofsteede.com>
2014-02-05 22:35:40 -0800

    order bidder form by name

--- auction/forms.py
+++ auction/forms.py
@@ -13,7 +13,7 @@ class BidderForm(forms.ModelForm):
         exclude = ('deleted')
 
 class PurchaseForm(forms.Form):
-    bidder = forms.ModelChoiceField(queryset=Bidder.objects.order_by('code'))
+    bidder = forms.ModelChoiceField(queryset=Bidder.objects.order_by('name'))
     unit_price = forms.FloatField(required=False) 
     quantity = forms.IntegerField(initial=1)
 
